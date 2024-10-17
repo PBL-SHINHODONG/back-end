589688bbf00151feddc632a0b05936fb1f1388e6
Gomdue <hdh026163@gmail.com>
2024-10-17 17:09:42 +0900

    fix: CORS 해결 및 github actions 코드 보완

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import places
+from app.routers import places
 
 app = FastAPI()
+
+origins = [
+    ["*"],
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(places.router)
 
